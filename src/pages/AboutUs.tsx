ad50a00ac1ccae6aceb09a198706a54db3aa650e
Lay7415 <89183158+Lay7415@users.noreply.github.com>
2023-09-17 12:46:20 +0000

    Update 2 files

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -4,7 +4,7 @@ import Banner from "../components/UI/Banner";
 import '../styles/pages/AboutUs/AboutUsFirstSection.css'
 import '../styles/pages/AboutUs/AboutUsSecondSection.css'
 import '../styles/pages/AboutUs/AboutUsThirdSection.css'
-import '../styles/pages/AboutUs/AboutUSFifthSection.css'
+import '../styles/pages/AboutUs/AboutUsFifthSection.css'
 import '../styles/pages/AboutUs/AboutUsSixthSection.css'
 import '../styles/pages/AboutUs/AboutUsSeventhSection.css'
 import { aboutUsS1Description, aboutUsS2Description, aboutUsS3Description, aboutUsS4Description, aboutUsS5Description, aboutUsS6Description, aboutUsS7Description } from "../constants/pages/AboutUs";
