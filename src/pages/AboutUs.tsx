31444ae63dedfa67996093fcc88d7f611caa2116
Lay7415 <ieye7531@gmail.com>
2023-09-17 19:26:53 +0600

    bug fix

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,18 +1,15 @@
-import React from "react";
 import MainLayout from "../components/layouts/MainLayout";
 import Banner from "../components/UI/Banner";
 import '../styles/pages/AboutUs/AboutUsFirstSection.css'
 import '../styles/pages/AboutUs/AboutUsSecondSection.css'
 import '../styles/pages/AboutUs/AboutUsThirdSection.css'
-import '../styles/pages/AboutUs/AboutUSFifthSection.css'
+import '../styles/pages/AboutUs/AboutUsFifthSection.css'
 import '../styles/pages/AboutUs/AboutUsSixthSection.css'
 import '../styles/pages/AboutUs/AboutUsSeventhSection.css'
 import { aboutUsS1Description, aboutUsS2Description, aboutUsS3Description, aboutUsS4Description, aboutUsS5Description, aboutUsS6Description, aboutUsS7Description } from "../constants/pages/AboutUs";
 import CardList from "../components/UI/CardList";
 
-type Props = {};
-
-const AboutUs = (props: Props) => {
+const AboutUs = () => {
   return <MainLayout>
     <Banner {...aboutUsS1Description} />
     <Banner {...aboutUsS2Description} />
